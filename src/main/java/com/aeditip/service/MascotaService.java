39ba16ca32de9ee8855b4972934efadecff42234
Stiven Medina <stvnm33@gmail.com>
2023-10-07 09:23:37 -0500

    Agregando el MascotaDTO al MascotaService

--- src/main/java/com/aeditip/service/MascotaService.java
+++ src/main/java/com/aeditip/service/MascotaService.java
@@ -1,8 +1,13 @@
 package com.aeditip.service;
 
 
+import com.aeditip.dto.MascotaDTO;
+import com.aeditip.model.Dueno;
 import com.aeditip.model.Mascota;
+import com.aeditip.model.Raza;
+import com.aeditip.repository.IDuenoRepository;
 import com.aeditip.repository.IMascotaRepository;
+import com.aeditip.repository.IRazaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +18,17 @@ public class MascotaService {
 
 
     private IMascotaRepository mascotaRepo;
+    private IDuenoRepository duenoRepository;
+    private IRazaRepository razaRepository;
 
 
-    public MascotaService(IMascotaRepository mascotaRepo) {
+    public MascotaService(IMascotaRepository mascotaRepo,
+                          IDuenoRepository duenoRepository,
+                          IRazaRepository razaRepository
+                          ) {
         this.mascotaRepo = mascotaRepo;
+        this.duenoRepository = duenoRepository;
+        this.razaRepository = razaRepository;
     }
 
     // Leer (R) SELECT * FROM mascota;
@@ -25,8 +37,21 @@ public class MascotaService {
     }
 
     // Crear (C) INSERT INTO mascota VALUES (...)
-    public Mascota crearMascota(Mascota mascota){
-        return mascotaRepo.save(mascota);
+    public Mascota crearMascota(MascotaDTO mascotaDTO){
+
+        Mascota mascota = new Mascota();
+
+        Dueno dueno = duenoRepository.findById(mascotaDTO.getIdDueno()).orElse(null);
+        Raza raza = razaRepository.findById(mascotaDTO.getIdRaza()).orElse(null);
+
+        mascota.setNombre(mascotaDTO.getNombre());
+        mascota.setDueno(dueno);
+        mascota.setRaza(raza);
+
+        Mascota mascotaCreada = mascotaRepo.save(mascota);
+
+        if(mascotaCreada != null) return mascotaCreada;
+        else throw new RuntimeException("No se pudo crear la mascota");
     }
 
     // Leer (R) SELECT * FROM mascota WHERE id_mascota = '?';
@@ -43,10 +68,12 @@ public class MascotaService {
     }
 
     // Eliminar fisicamente (D) DELETE FROM mascota WHERE id_mascota = '?';
-    public void eliminarMascota(int id){
-        if(mascotaRepo.findById(id).isPresent()){
+    public boolean eliminarMascota(int id) {
+        if (mascotaRepo.findById(id).isPresent()) {
             mascotaRepo.deleteById(id);
+            return true; // Eliminación exitosa
         }
+        return false; // La mascota no se encontró o no se pudo eliminar
     }
 
 
